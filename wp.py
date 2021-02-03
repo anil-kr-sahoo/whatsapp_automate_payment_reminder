a0dfbee01fe96a7c75ceb64094dcb61e39d0fb1d
Anil Kumar Sahoo <31738270+anil01081997@users.noreply.github.com>
2021-02-03 13:36:48 +0530

    Add files via upload
    Initial commit

---
+++ wp.py
@@ -0,0 +1,26 @@
+
+pending_dues = {"+917749984274":"10", "+917008308150":"10"}
+
+def get_current_time():
+	import datetime
+	current_time_hour = datetime.datetime.now().hour
+	current_time_minute = datetime.datetime.now().minute
+	return current_time_hour,current_time_minute
+
+def minute_check(current_time_hour, current_time_minute):
+	current_time_minute += 2
+	if current_time_minute >=60:
+		current_time_minute = current_time_minute - 60
+		current_time_hour +=1
+		if current_time_hour == 24:
+			current_time_hour = 00
+	return current_time_hour, current_time_minute
+
+
+current_time_hour, current_time_minute=get_current_time()
+current_time_hour, current_time_minute=minute_check(current_time_hour, current_time_minute)
+import pywhatkit	
+for number, amount in pending_dues.items():
+	amount_message = "Hello dear, It's a gentle reminder that you have pending due of Rs."+amount+"/-. \nYou can pay through 'contribute' section through our app."
+	pywhatkit.sendwhatmsg(number,amount_message,current_time_hour,current_time_minute)
+	current_time_hour, current_time_minute=minute_check(current_time_hour, current_time_minute)
